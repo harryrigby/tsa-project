5a42629a37324bf6f76674914fc140306ff6f6cd
harryrigby <96383045+harryrigby@users.noreply.github.com>
2022-02-25 16:29:07 +0000

    Create Chapter 1.R
    Chapter 1 R code

---
+++ Chapter 1.R
@@ -0,0 +1,11 @@
+##############################################
+### TIME SERIES ANALYSIS: CHAPTER 1 R CODE ###
+##############################################
+
+# Figure 1.1: AMZN close stock price plot
+install.packages('quantmod') #'quantmod' package used to import data into R
+require('quantmod')
+getSymbols('AMZN', src='yahoo') # Source: Yahoo Finance
+par(mfrow=c(1,1)) 
+AMZN <- ts(AMZN$AMZN.Close, start=2007, frequency = 253) # Setting time scale
+tsplot(AMZN, ylab="Close Price (USD$)", col="red")
