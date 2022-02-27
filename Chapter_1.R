3675e85e9775ff8eeafdaead7c4a94fa3edd2376
harryrigby <96383045+harryrigby@users.noreply.github.com>
2022-02-27 16:41:10 +0000

    Update Chapter_1.R

--- Chapter_1.R
+++ Chapter_1.R
@@ -10,3 +10,9 @@ getSymbols('AMZN', src='yahoo') # Source: Yahoo Finance
 par(mfrow=c(1,1)) 
 AMZN <- ts(AMZN$AMZN.Close, start=2007, frequency = 253) # Setting time scale
 tsplot(AMZN, ylab="Close Price (USD$)", col="red")
+
+# Figure 1.3: MSFT daily returns
+getSymbols('MSFT', src='yahoo')
+p <- ts(MSFT$MSFT.Close, start=2007, frequency = 253)
+r <- diff(log(p))
+tsplot(r, col="red", ylim=c(-0.18,0.18))
